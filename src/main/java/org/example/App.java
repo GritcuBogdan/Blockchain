c0001a3042bd9710695a3442018051359c9cdf1f
unknown <gritcub@gmail.com>
2025-01-20 18:49:18 +0200

    adding wallets and transactions table via Hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example")
+@EntityScan(basePackages ={"org.example.Entity"})
 public class App {
     public static void main(String[] args) {
        SpringApplication.run(App.class,args);
---
+++ src/main/java/org/example/Entity/Wallet.java
@@ -0,0 +1,79 @@
+package org.example.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wallets")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long wallet_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Double balance;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+
+    public Wallet(Long wallet_id, User user, Double balance, LocalDateTime created_at, LocalDateTime updated_at) {
+        this.wallet_id = wallet_id;
+        this.user = user;
+        this.balance = balance;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public Wallet() {
+
+    }
+
+    public Long getWallet_id() {
+        return wallet_id;
+    }
+
+    public void setWallet_id(Long wallet_id) {
+        this.wallet_id = wallet_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+}
