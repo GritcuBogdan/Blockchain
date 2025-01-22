8abe676afe338c2802236e51d26d0c44eaaf7e3a
unknown <gritcub@gmail.com>
2025-01-22 19:27:32 +0200

    Basic APIs for the wallet entity

--- src/main/java/org/example/Repository/WalletRepository.java
+++ src/main/java/org/example/Repository/WalletRepository.java
@@ -2,9 +2,16 @@ package org.example.Repository;
 
 import org.example.Entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-
+    @Query("SELECT w FROM Wallet w WHERE w.walletId = :walletId")
+    Optional<Wallet> getWalletByWalletId(@Param("walletId") Long walletId);
+    public List<Wallet> findByUserId(Long userId);
 }
