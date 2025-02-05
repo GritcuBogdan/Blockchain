cb353eb7646f4f81b80186d584bcd0467b49fba8
unknown <gritcub@gmail.com>
2025-02-05 20:56:35 +0200

    ÈÐLogin Button. Sign in / Sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,22 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import "./index.css";
 import "./global/global.scss";
 import Home from "./pages/Home.jsx";
-import RegisterPage from "./pages/Register.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 function App() {
+    const location = useLocation();
+    const showFooter = location.pathname !== "/login";
+
     return (
         <div className="font-mono">
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
-            <Footer />
+            {showFooter && <Footer />}
         </div>
     );
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Login = () => {
+    const [activeTab, setActiveTab] = useState("signin");
+    const [currency, setCurrency] = useState("");
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                dropdownRef.current.removeAttribute("open");
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="w-full max-w-lg mb-40">
+                <div
+                    role="tablist"
+                    className="tabs tabs-boxed mb-5 border border-white rounded-md justify-between items-center px-18"
+                >
+                    <button
+                        role="tab"
+                        className={`tab ${activeTab === "signin" ? "tab-active" : ""}`}
+                        onClick={() => setActiveTab("signin")}
+                        style={{ color: "white" }}
+                    >
+                        Sign In
+                    </button>
+                    <button
+                        role="tab"
+                        className={`tab ${activeTab === "signup" ? "tab-active" : ""}`}
+                        onClick={() => setActiveTab("signup")}
+                        style={{ color: "white" }}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div className="relative h-[420px]">
+                    <div
+                        className={`card border border-white shadow-md p-6 absolute w-full transition-opacity duration-300 ${
+                            activeTab === "signin"
+                                ? "opacity-100 visible"
+                                : "opacity-0 invisible"
+                        }`}
+                    >
+                        <h2 className="text-xl font-semibold mb-2" style={{ color: "white" }}>
+                            Log in to your account
+                        </h2>
+                        <p className="text-sm text-gray-500 mb-4">
+                            Enter your email and password to log in.
+                        </p>
+                        <form className="space-y-4">
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                className="input input-bordered w-full bg-transparent text-gray-100"
+                            />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="input input-bordered w-full bg-transparent text-gray-100"
+                            />
+                            <a
+                                href="/reset-password"
+                                className="text-sm text-primary block text-center"
+                            >
+                                Forgot your password?
+                            </a>
+                            <button className="btn btn-primary w-full bg-transparent">
+                                Log In
+                            </button>
+                        </form>
+                    </div>
+                    <div
+                        className={`card border border-white shadow-md p-6 absolute w-full transition-opacity duration-300 ${
+                            activeTab === "signup"
+                                ? "opacity-100 visible"
+                                : "opacity-0 invisible"
+                        }`}
+                    >
+                        <h2 className="text-xl font-semibold mb-2 text-white">Create a New Account</h2>
+                        <p className="text-sm text-gray-500 mb-4">
+                            Enter your details to create a new account.
+                        </p>
+                        <form className="space-y-4">
+                            <input
+                                type="text"
+                                placeholder="First Name"
+                                className="input input-bordered w-full bg-transparent text-gray-100"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Last Name"
+                                className="input input-bordered w-full bg-transparent text-gray-100"
+                            />
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                className="input input-bordered w-full bg-transparent text-gray-100"
+                            />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="input input-bordered w-full bg-transparent text-gray-100"
+                            />
+                            <input
+                                type="password"
+                                placeholder="Confirm Password"
+                                className="input input-bordered w-full bg-transparent"
+                            />
+                            <details
+                                className="dropdown bg-transparent w-full"
+                                ref={dropdownRef}
+                            >
+                                <summary className="btn bg-transparent w-full border border-gray-600 rounded-md text-white">
+                                    {currency ? currency : "Select Currency"}
+                                </summary>
+                                <ul className="menu w-full dropdown-content rounded-box z-[1] p-2 shadow border border-white text-gray-100 bg-[#222222]">
+                                    <li onClick={() => setCurrency("USD")}>
+                                        <a>USD</a>
+                                    </li>
+                                    <li onClick={() => setCurrency("EUR")}>
+                                        <a>EUR</a>
+                                    </li>
+                                    <li onClick={() => setCurrency("GBP")}>
+                                        <a>GBP</a>
+                                    </li>
+                                </ul>
+                            </details>
+
+                            <button className="btn btn-primary w-full bg-transparent text-white">
+                                Sign Up
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
